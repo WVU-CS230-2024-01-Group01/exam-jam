c1581cae8522873330c5fb31de82bc6a559db54d
jrb00013 <156119500+jrb00013@users.noreply.github.com>
2024-04-14 14:44:22 -0400

    Updated Home.js with applied styles from Home.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,13 @@ import { useNavigate } from "react-router-dom";
 
 const Home = () => {
 
-    let foundUser = localStorage.getItem('user');
+    let foundUser = localStorage.getItem('user')
     foundUser = localStorage.getItem('user');
-    const navigate = useNavigate();
+    const navigate = useNavigate()
 
     function handleLogout () {
-        localStorage.clear();
-        navigate('/login');
+        localStorage.clear()
+        navigate('/login')
     }
 
     const classData = [ //for use in cards
@@ -20,7 +20,6 @@ const Home = () => {
         { title: "CS320", description: "Analysis of Algorithms" },
         { title: "MATH251", description: "Calculus III" }
     ];
-    
 
     return (
         <div>
@@ -32,19 +31,22 @@ const Home = () => {
             />
             <div className="hero">
                 <nav>
-                    <h2 className="logo">ExamJam</h2>
-                    <h1>Welcome {foundUser}</h1>
-
+                    
+                    <img className="logo" src="/logoimage.jpeg" alt="ExamJam Logo" width="451.5px"  length="164.25px"/>
+                    <h1 className="welcome">Welcome {foundUser}</h1>
 
                     {foundUser ? (
                         <>
-                            <Link to="/profilepage"><button>Edit Profile</button></Link>
-                            <Link to="/logout"><button onClick={handleLogout}>Logout</button></Link>
-                            <Link to="/studysets"><button>Study Sets</button></Link>
+                            <Link to="/profilepage"><button className="profileBar"> Edit Profile </button></Link>
+                            <Link to="/logout"><button onClick={handleLogout} className="profileBar"> Logout </button></Link>
+                            <Link to="/studysets"><button className="profileBar">Study Sets</button> </Link>
                         </>
                     ) : (
-                        <Link to="/login"><button>Login</button></Link>
+                       <Link to="/login"><button className="profileBar"> Login </button></Link>
                     )}
+
+                   
+                
                    
                     </nav>
                 <div className="search-container">
@@ -52,7 +54,7 @@ const Home = () => {
                 </div>
 
                 <div id="classesBox">
-         
+                    <div className="behindClassesBox"> </div>
                     {classData.map((classItem, index) => ( //iterates over each item in classData and passes its information to a Card
                         <Card className = "card"
                             key={index} // Providing a unique key for each card
@@ -60,6 +62,7 @@ const Home = () => {
                             description={classItem.description}
                         />
                     ))}
+                   
 
                 </div>
 
