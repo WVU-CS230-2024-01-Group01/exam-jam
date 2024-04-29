5a7a75d3f2c34579364236a9e6cff0a811976583
jrb00013 <156119500+jrb00013@users.noreply.github.com>
2024-04-29 11:01:15 -0400

    Update CreateStudySets.jsx

--- src/pages/CreateStudySets.jsx
+++ src/pages/CreateStudySets.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import "./StudySetStyles.css";
+import "./LoginStyles.css";
 
 const CreateStudySets = () =>{
     let username = localStorage.getItem('user')
